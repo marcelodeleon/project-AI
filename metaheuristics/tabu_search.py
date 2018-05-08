f09274d51d6818f32350bc70411ee173926b2ff4
marcelodeleon <mdeleoncosentino@gmail.com>
2018-05-08 18:52:06 -0300

    Fixed flake8 warnings

--- metaheuristics/tabu_search.py
+++ metaheuristics/tabu_search.py
@@ -1,16 +1,22 @@
 # -*- coding: utf-8 -*-
 
+
 def surroundings(center, radius, domains):
-    """ The surroundings of a `center` is a list of new centers, all equal to the center except for
-    one value that has been increased or decreased by `radius`.
     """
+    The surroundings of a `center` is a list of new centers, all equal to the
+    center except for one value that has been increased or decreased by
+    `radius`.
+    """
+
     return [center[0:i] + (center[i] + d,) + center[i + 1:]
-               for i in range(len(center)) for d in (-radius, +radius)
-               if center[i] + d >= domains[i][0] and center[i] + d <= domains[i][1]
-           ]
+            for i in range(len(center)) for d in (-radius, +radius)
+            if center[i] + d >= domains[i][0] and
+            center[i] + d <= domains[i][1]]
+
 
 def tabu_search(problem, steps=100, delta=1, initial=None, tabuSize=5):
     """ Tabu Search optimization implemented as a generator function."""
+
     current = initial or problem.randomElement()
     lastEval = problem.objective(current)
     current = (current, lastEval)
@@ -27,14 +33,24 @@ def tabu_search(problem, steps=100, delta=1, initial=None, tabuSize=5):
         bestCandidate = neighborHood[0]
 
         for candidate in neighborHood:
-            if candidate not in tabuList and problem.compareEvaluations(problem.objective(bestCandidate), problem.objective(candidate)) > 0:
+            if candidate not in tabuList and \
+                    problem.compareEvaluations(
+                        problem.objective(bestCandidate),
+                        problem.objective(candidate)
+                    ) > 0:
                 bestCandidate = candidate
-        if problem.compareEvaluations(problem.objective(best), problem.objective(bestCandidate)) > 0:
+
+        if problem.compareEvaluations(
+            problem.objective(best),
+            problem.objective(bestCandidate)
+        ) > 0:
             best = bestCandidate
+
         tabuList.append(bestCandidate)
         if len(tabuList) > tabuSize:
             tabuList.pop(0)
         yield (best, problem.objective(best))
+
 
 def sum_squares():
     from .test_problems import SUM_SQUARES
