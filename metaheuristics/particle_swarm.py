35458a634887fe10150f887e9acd701cc457412e
marcelodeleon <mdeleoncosentino@gmail.com>
2018-05-09 17:26:31 -0300

    WIP Finished optimized, haven't plugged it in yet

--- metaheuristics/particle_swarm.py
+++ metaheuristics/particle_swarm.py
@@ -1,12 +1,15 @@
 import random
 
-# Particle Swarm coefficients.
+# Particle swarm coefficients.
 W = 0.729844  # Inertia.
 C1 = 1.496180  # Cognitive acceleration.
 C2 = 1.496180  # Social acceleration.
 
-# Population size
-SWARM_SIZE = 30
+# Configuration constants.
+STEPS = 100
+SWARM_SIZE = 40
+
+# TODO: Remove useless prints.
 
 
 class Particle:
@@ -24,8 +27,8 @@ class Particle:
             Representation of the problem to be optimized.
         """
 
-        # Particle's memory, indicates the best position (with it's associated
-        # fitness) found so far.
+        # Particle's memory, indicates the best['position'] (with it's
+        # associated fitness) found so far.
         self.best = {
             'fitness': None,
             'position': None
@@ -52,14 +55,14 @@ class Particle:
         Parameters
         ----------
         gBest: tuple
-            Best position from the swarm so far.
+            best['position'] from the swarm so far.
         """
 
         R2 = random.random()
 
         return tuple(map(
             lambda x, y: C2 * R2 * (x - y),
-            zip(gBest, self.position)
+            zip(gBest['position'], self.position)
         ))
 
     def _get_cognitive_term(self):
@@ -71,15 +74,25 @@ class Particle:
 
         return tuple(map(
             lambda x, y: C1 * R1 * (x - y),
-            zip(self.pos_best, self.position)
+            zip(self.best['position'], self.position)
         ))
+
+    def _get_inertia_term(self):
+        """
+        Get the inertia term from the particle velocity equation.
+        """
+
+        return tuple(map(lambda x: W * x, self.velocity))
 
     def update_position(self):
         """
-        Calculate the new position using the latest velocity.
+        Calculate the new position using the latest velocity vector.
         """
 
-        self.position = tuple(map(lambda x: x + self.velocity), self.position)
+        self.position = tuple(map(
+            lambda x, y: x + y,
+            zip(self.position, self.velocity)
+        ))
 
     def update_velocity(self, gBest):
         """
@@ -88,30 +101,33 @@ class Particle:
         Parameters
         ----------
         gBest: tuple
-            Best position from the swarm so far.
+            best['position'] from the swarm so far.
         """
 
+        inertia_term = self._get_inertia_term()
         social_term = self._get_social_term(gBest)
         cognitive_term = self._get_cognitive_term()
 
         self.velocity = tuple(map(
-            lambda x, y, z: W * x + y + z,
-            zip(self.velocity, social_term, cognitive_term)
+            lambda x, y, z: x + y + z,
+            zip(inertia_term, social_term, cognitive_term)
         ))
 
     def __repr__(self):
         return 'Position: {}\nFitness: {}'.format(self.position, self.fitness)
 
     def satisfy_constraints(self):
-        # This is where constraints are satisfied
+        # This is where constraints are satisfied. Using the 'Let it fly'
+        # strategy.
         return
 
 
 class PSO:
-    solucion = []
-
-    def __init__(self, problem, steps):
+    def __init__(self, problem, steps=STEPS, swarm_size=SWARM_SIZE):
         self.swarm = []
+
+        # Number of particles in the swarm.
+        self.swarm_size = swarm_size
 
         # Definition of the optimization problem.
         self.problem = problem
@@ -119,43 +135,82 @@ class PSO:
         # Number of steps for the algorithm.
         self.steps = steps
 
-        for _ in range(SWARM_SIZE):
+        # best['position'] for the whole swarm.
+        self.gBest = {
+            'fitness': -self.problem.target,
+            'position': None
+        }
+
+        # Initialize list with all particles (Swarm).
+        for p in range(self.swarm_size):
             particle = Particle(problem)
             self.swarm.append(particle)
+
+            # Update global best
+            if self._current_better_than_global_best(particle):
+                self.gBest['position'] = particle.position
+                self.gBest['fitness'] = particle.fitness
 
         for p in self.swarm:
             print(p)
 
-    # def optimize(self):
-    #     for i in range(iteraciones):
-    #         print "iteracion ", i
-    #         # Obtiene el mejor global
-    #         globalBest = self.swarm[0]
-    #         for j in range(swarmSize):
-    #             posBest = self.swarm[j].posBest
-    #             if self.f(posBest) > self.f(globalBest):
-    #                 globalBest = posBest
-    #                 solution = globalBest
-    #                 # Acutalizo la posición de la particula
-    #                 for k in range(swarmSize):
-    #                     self.swarm[k].ActualizaVel(globalBest)
-    #                     self.swarm[k].ActualizaPos()
-    #                     self.swarm[k].satisfyConstraints()
-    #                     # Actualiza el personal best
-    #                     for l in range(swarmSize):
-    #                         posBest = self.swarm[l].posBest
-    #                         if self.f(self.swarm[l]) > self.f(posBest):
-    #                             self.swarm[l].posBest = self.swarm[l].position_i
-    #                             return solution
+        print('Global Best so far is {}, fitness: {}'.format(
+            self.gBest['position'],
+            self.gBest['fitness']
+        ))
 
-    # def f(self, solution):
-    #     return random.random()
+    def _current_better_than_personal_best(self, current_particle):
+        """
+        Checks if the current particle's position is better than the
+        personal best known.
+        """
+
+        return self.problem.compareEvaluations(
+                   current_particle.best['fitness'],
+                   current_particle.fitness
+               ) > 0
+
+    def _current_better_than_global_best(self, current_particle):
+        """
+        Checks if the current particle's position is better than the global
+        best known.
+        """
+
+        return self.problem.compareEvaluations(
+                   self.gBest['fitness'],
+                   current_particle.fitness
+               ) > 0
+
+    def optimize(self):
+        """PSO Main loop."""
+
+        for i in range(self.steps):
+            print('Step', i)
+
+            for p in range(self.swarm_size):
+                current_particle = self.swarm[p]
+
+                # Update velocity and position for current particle.
+                current_particle.update_velocity(self.gBest)
+                current_particle.update_position()
+                current_particle.satisfy_constraints()
+
+                # Update best['position'] for current particle.
+                if self._current_better_than_personal_best(current_particle):
+                    current_particle.best['fitness'] = current_particle.fitness
+                    current_particle.best['position'] = \
+                        current_particle.position
+
+                    # Update global best.
+                    if self._current_better_than_global_best(current_particle):
+                        self.gBest['position'] = current_particle.position
+                        self.gBest['fitness'] = current_particle.fitness
 
 
 def sum_squares():
     from .test_problems import SUM_SQUARES
     problem = SUM_SQUARES
-    PSO(problem, steps=10000)
+    PSO(problem)
 
 sum_squares()
 # def eggholder():
