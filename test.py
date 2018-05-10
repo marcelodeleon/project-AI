07ec037f1f5e29b2e9da7cb97fdb071957cd99a3
dimartiro <diego2737@gmail.com>
2018-05-09 21:52:32 -0300

    Stats fixes

--- test.py
+++ test.py
@@ -3,14 +3,14 @@ from metaheuristics import *
 TEST_TIMES = 50
     
 def calc_stats(meta1, meta2):
-    print("{} {:>30} {:>30} {:>30}".format(meta1.__name__ + "_fitness", meta1.__name__ + "_evaluations", meta2.__name__ + "_fitness", meta2.__name__ + "_evaluations"))
+    print("{} {:>45} {:>45} {:>45}".format(meta1.__module__ + "_fitness", meta1.__module__ + "_evaluations", meta2.__module__ + "_fitness", meta2.__module__ + "_evaluations"))
     for i in range(TEST_TIMES):
-        print("{:>19} {:>30} {:>30} {:>30}".format(meta1()[1], meta1()[2], meta2()[1], meta2()[2]))
+        print("{:>35} {:>45} {:>45} {:>45}".format(meta1()[1], meta1()[2], meta2()[1], meta2()[2]))
 
 def print_title(title):
-    print("-"*112)
+    print("-"*180)
     print("{:^100}".format(title))
-    print("-"*112)
+    print("-"*180)
 
 #TEST SUM_SQUARES
 print_title("SUM_SQUARES")
